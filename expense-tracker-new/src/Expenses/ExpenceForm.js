ff8d332bba372ec503f92578ecb362125f83ab19
amarsrivastava68 <70334464+amarsrivastava68@users.noreply.github.com>
2023-10-31 22:42:41 +0530

    redux configuration changes

--- expense-tracker-new/src/Expenses/ExpenceForm.js
+++ expense-tracker-new/src/Expenses/ExpenceForm.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import './ExpenseForm.css';
+import authReducer from "../Store/authReducer";
 import { useDispatch } from 'react-redux';
-import { addExpense } from '../Store/ExpenseReducer'; 
-
+import { addExpenseS } from '../Store/ExpenseReducer'; 
+import { useSelector } from "react-redux";
 const ExpenceForm = ({ addExpense }) => {
+  const userId = useSelector((state) => state.auth.userId);
+
+  const extracted =  userId?.replace('@', '').replace('.', '');
+  console.log(userId)
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  
+  console.log(isLoggedIn)
   const [amount, setAmount] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("Choose...");
@@ -14,14 +22,14 @@ const ExpenceForm = ({ addExpense }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const StoreUrl = 'https://authentication-66cfd-default-rtdb.firebaseio.com/expenses.json';
+    const StoreUrl = `https://authentication-66cfd-default-rtdb.firebaseio.com/expenses/${extracted}.json`;
 
     const expense = {
       amount: parseFloat(amount),
       description,
       category,
     };
-
+console.log(expense)
     fetch(StoreUrl, {
       method: 'POST',
       headers: {
@@ -36,11 +44,15 @@ const ExpenceForm = ({ addExpense }) => {
         return response.json();
       })
       .then(responseData => {
-        console.log('Expense details stored successfully', responseData);
+        // console.log('response data is ' , responseData)
+        // console.log('Expense details stored successfully', responseData);
         const id = responseData.name;
-        console.log(id);
-        dispatch(addExpense(expense));
-        addExpense({ id, ...expense });
+        console.log('this is id - ' , id);
+        console.log('this is expense object '  , expense)
+        console.log(addExpenseS)
+        dispatch(addExpenseS({ id, ...expense }));
+        
+       // addExpense({ id, ...expense });
       })
       .catch(error => {
         console.log('Error updating user details', error);
